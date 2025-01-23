f990a95a5232f96d0789eac8892834d4f4c1fad8
bgautham4 <bgautham4@gmail.com>
2025-01-23 12:40:35 +0530

    Remaining files

---
+++ src/request.cpp
@@ -0,0 +1,3 @@
+#include "request.h"
+
+request_id_t Request::next_id = 0;
---
+++ src/request.h
@@ -0,0 +1,20 @@
+#ifndef REQUEST_H
+#define REQUEST_H
+
+#include "units.h"
+using request_id_t = int;
+
+struct Request {
+	Request(int input_len, int output_len) : id(++next_id), num_input_tokens(input_len), num_output_tokens(output_len) {}
+	request_id_t id;
+	int num_input_tokens;
+	int num_output_tokens;
+	int num_tokens_generated = 0;
+	simtime_ps_t arrival_time = 0;
+	simtime_ps_t first_scheduled_time = 0;
+	simtime_ps_t departure_time = 0;
+private:
+	static request_id_t next_id;
+};
+
+#endif
